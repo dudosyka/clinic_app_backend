f037274714f7eb5b04b8f608e8c78e0288a63d38
MixaDev <20299694+TheMixaDev@users.noreply.github.com>
2023-02-08 00:02:39 +0300

    delete deprecated models

--- src/modules/appointment/appointment.module.ts
+++ src/modules/appointment/appointment.module.ts
@@ -4,23 +4,6 @@ import { AppointmentModel } from './models/appointment.model';
 import { AppointmentService } from './services/appointment.service';
 import { AppointmentController } from './controllers/appointment.controller';
 import { UserModule } from '../user/user.module';
-import { AbTherapyReference } from './models/references/ab-therapy.reference';
-import { DiabetesMelitusReference } from './models/references/diabetes-melitus.reference';
-import { EyeDiseaseReference } from './models/references/eye-disease.reference';
-import { GeneCarriageReference } from './models/references/gene-carriage.reference';
-import { GsdReference } from './models/references/gsd.reference';
-import { HemodinDisordersReference } from './models/references/hemodin-disorders.reference';
-import { IvfReference } from './models/references/ivf.reference';
-import { IvigReference } from './models/references/ivig.reference';
-import { NmhReference } from './models/references/nmh.reference';
-import { OagaReference } from './models/references/oaga.reference';
-import { ObesityReference } from './models/references/obesity.reference';
-import { PlasmapharesisReference } from './models/references/plasmapharesis.reference';
-import { TwinsReference } from './models/references/twins.reference';
-import { UternusScarReference } from './models/references/uternus-scar.reference';
-import { VaccineMicrofloraReference } from './models/references/vaccine.microflora.reference';
-import { VaccineLocalizationReference } from './models/references/vaccine-localization.reference';
-import { VaccineValueReference } from './models/references/vaccine-value.reference';
 import { ReferencesService } from './services/references.service';
 import { ReferencesController } from './controllers/references.controller';
 import {MulterModule} from "@nestjs/platform-express";
@@ -31,25 +14,7 @@ import {UserFilesModel} from "../user/models/user-files.model";
   imports: [
     SequelizeModule.forFeature([
       AppointmentModel,
-
-      AbTherapyReference,
-      DiabetesMelitusReference,
-      EyeDiseaseReference,
-      GeneCarriageReference,
-      GsdReference,
-      HemodinDisordersReference,
-      IvfReference,
-      IvigReference,
-      NmhReference,
-      OagaReference,
-      ObesityReference,
-      PlasmapharesisReference,
-      TwinsReference,
-      UternusScarReference,
-      VaccineMicrofloraReference,
-      VaccineLocalizationReference,
-      VaccineValueReference,
-        UserFilesModel
+      UserFilesModel
     ]),
     MulterModule.registerAsync({
       useClass: MulterConfigModule
